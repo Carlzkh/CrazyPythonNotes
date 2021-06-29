aeb86d55235343b5a9dd2c0a6ececc6efb9ab786
kunhe.zhang <291747808@qq.com>
2021-06-29 10:01:32 +0800

    202106281

--- data_visualization/matplotlib/barh_test.py
+++ data_visualization/matplotlib/barh_test.py
@@ -7,7 +7,7 @@ y_data2 = [52000, 54200, 51500, 58300, 56800, 59500, 62700]
 bar_width = 0.3
 plt.barh(y=range(len(x_data)), width=y_data, label='疯狂Java讲义', color='steelblue', alpha=0.8, height=bar_width)
 plt.barh(y=np.arange(len(x_data))+bar_width+0.05, width=y_data2, label='疯狂Android讲义', color='indianred',
-        alpha=0.8, height=bar_width)
+         alpha=0.8, height=bar_width)
 
 # 在柱状图上显示具体 数值， ha 参数控制水平对齐方式， va 参数控制垂直对齐方式
 for x, y in enumerate(y_data):
